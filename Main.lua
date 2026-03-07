4e58a6e5cab9c72b96ce205b15d0a2335072d8f6
astrxvian-stack <astrxvian@gmail.com>
2026-03-07 22:10:38 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,5 @@
+repeat wait() until game:IsLoaded()
 
+if game.PlaceId == 131767617769624 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/astrxvian-stack/SeaHub/refs/heads/main/AnimeFinals.lua"))()
+end
